46c2661e2adc2199d357a0e30dc900e9b4765a34
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-18 23:46:34 -0500

    Located and implemented basic setup for a jQuery plugin countdown timer with event that HTML CSS friendly.

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
 /* global $ */
 
-// $(document).ready(function() {
-//
-// });
+$(document).ready(function () {
 
-// var worker = null;
-//
-// function createWorkers() {
-//   if (worker === null) {
-//     // this needs to be in a different file
-//     // worker always has to have a separate file
-//     worker = new Worker('worker.js');
-//   }
-//   // add a listener here. this is needed, if you receive any data from the worker thread, you can handle it here
-//   worker.addEventListener('message', function(e) {
-//     // I am using -1 to indicate that the timer has finished, you can  use anything you want.
-//     if (parseInt(e.data) === -1) {
-//       alert('Your time is finished');
-//       // exit the thread
-//       $('#polling').trigger('click');
-//     } else {
-//       // update your timer text here. This is used to display the countdown timer
-//       $('#timer span').text(e.data);
-//     }
-//   }, false);
-// }
+  var d1 = new Date();
+  var d2 = new Date();
 
-  // $(document).on('click', '#startTimer', function() {
-  //   // create the workers here
-  //   createWorkers();
-  //   $.ajax({
-  //     url: '',
-  //     type: 'GET',
-  //     data: getData,
-  //     success: function(data, textStatus, jqXHR) {
-  //       if (data.data) {
-  //         // convert minutes into proper format
-  //         var min = $('#timer').val() + ':00';
-  //         var totalTime = $('#timer').val();
-  //         // send a message
-  //         worker.postMessage('timer:' + totalTime);
-  //       }
-  //     }
-  //   });
-//});
+  d2.setMinutes(d1.getMinutes() + 25);
+
+  console.log(d1);
+  console.log(d2);
+
+  //'2016/01/01'
+
+  $('.timer').countdown(d2, function (event) {
+    // $(this).text(event.strftime('%D days %H:%M:%S')
+    //   );
+    $(this).text(event.strftime('%M:%S')
+      );
+  });
+
+});
