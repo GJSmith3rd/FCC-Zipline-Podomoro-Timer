4b88b2236dcb5a4ab552356725df2af143cf7833
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-19 01:52:45 -0500

    Testing timers from mobile dev setup.

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ $(document).ready(function () {
 
   var d1 = new Date();
   var d2 = new Date();
+  var sess = 25;
 
-  d2.setMinutes(d1.getMinutes() + 25);
+  d2.setMinutes(d1.getMinutes() + sess);
 
   console.log(d1);
   console.log(d2);
+  
+  setTimers();
+  startTimer();
 
-  //'2016/01/01'
-
-  $('.timer').countdown(d2, function (event) {
-    // $(this).text(event.strftime('%D days %H:%M:%S')
-    //   );
-    $(this).text(event.strftime('%M:%S')
+function startTimer(){
+$('.timer').countdown(d2, function (event) {
+    
+    $(this).html(event.strftime('<span>%M</span> min ' + '<span>%S</span> sec')
       );
   });
+}  
+
+function setTimers(){ 
+
+}
 
 });
+
