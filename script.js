9dfaaf072b2692d47c77eed7a21df41678c9aaba
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-25 17:38:23 -0500

    Working on a problem with timer where the display pauses but the actual timer does not.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function () {
 
+  var resumeMin;
+  var pauseMin;
+  var duration = new Date();
+
+  // set url for sounds on google drive public
   var soundLocation = 'https://7b313a9f7606490ebe3c2c7d078512212b272396.googledrive.com/';
   soundLocation += 'host/0BxHL3kJgWo5eT1NpOW8zbzB0SzQ/sounds/';
 
@@ -22,140 +27,161 @@ $(document).ready(function () {
 
   var $clock = $('.timer');
 
-  var sessTime = 3;
-  var sessBreak = 2;
+  // initially set default timer variables
+  var sessTime = 25;
+  var sessBreak = 1;
   var setTime = 1;
-  var setBreak = 2;
+  var setBreak = 1;
 
-  //invoke pomodor timer bases on variables
-
+  /*
+  MAIN POMODORO DRIVER
+  */
   setPomodoro(sessTime, sessBreak, setTime, setBreak);
 
+  /*
+  TIMER SET FUNCTION
+  */
   function setPomodoro(sessTime, sessBreak, setTime, setBreak) {
 
-    setClock(setTime);
+    duration = setTimers(sessTime);
 
-    /*calculate sessions and breaks
-      and set clock
-    */
-    function setClock(clockTime) {
-
-      // set timer variables
-
+    //  compute session time
+    function setTimers() {
       var currentTime = new Date();
       var timerTime = new Date();
-
-      //  compute session time
-      timerTime.setMinutes(currentTime.getMinutes() + clockTime);
-
-      $($clock).countdown(timerTime)
-        .on('update.countdown', function (event) {
-          $.ionSound.play('snap', { volume: 0.25 });
-          $(this).html(event.strftime('<span>%M</span> min ' + '<span>%S</span> sec'));
-        })
-
-        .on('finish.countdown', function (event) {
-          $(this).html(event.strftime('<span>%M</span> min ' + '<span>%S</span> sec'));
-
-          $.ionSound.play('bell_ring', { volume: 0.1, loop:5 });
-
-          // .on('stop.countdown', function (event) {
-
-          // });
-
-          $('#timer-resume').addClass('disabled');
-
-        });
-
-      function setTimers() {
-        var d1 = new Date();
-        var d2 = new Date();
-        return d2.setMinutes(d1.getMinutes() + sessTime);
-      }
-
-      $('#timer-reset').click(function () {
-        $clock.countdown(setTimers());
-        $('#timer-resume').removeClass('disabled');
-        $('#timer-resume').removeClass('active');
-        $('#timer-pause').removeClass('disabled');
-        $('#timer-pause').removeClass('active');
-      });
-
-      $('#timer-pause').click(function () {
-        $clock.countdown('pause');
-        $(this).addClass('disabled');
-        $('#timer-resume').removeClass('disabled');
-      });
-
-      $('#timer-resume').click(function () {
-        $clock.countdown('resume');
-        $(this).addClass('disabled');
-        $('#timer-pause').removeClass('disabled');
-      });
+      return timerTime.setMinutes(currentTime.getMinutes() + sessTime);
     }
-    // call immediate function to close panels
-    (function () {
-      $('.panel')
+
+    /*
+    TIMER EVENTS
+    */
+
+    $clock.countdown(duration, function (event) {
+      $(this).
+        html(event.strftime('<span>%M</span> min ' + '<span>%S</span> sec'));
+    })  //finish
+      .on('finish.countdown', function (event) {
+        $.ionSound.play('bell_ring', { volume: 0.1, loop: 5 });
+      })//stop
+    // .on('stop.countdown', function (event) {
+    //   $.ionSound.play('bell_ring', { volume: 0.1, loop: 5 });
+    // })//update
+      .on('update.countdown', function (event) {
+        $.ionSound.play('snap', { volume: 0.25 });
+      });
+
+    /*
+    TIMER CONTROLS
+    */
+
+    $('#timer-resume').click(function () {
+      //resume timer
+      //$clock.countdown('resume');
+
+      resumeMin = new Date();
+
+      var timedelta = resumeMin.getTime() - pauseMin.getTime();
+
+      console.log(resumeMin.toString());
+      $clock.countdown(timedelta.parseTime());
+
+      $(this).addClass('disabled');
+      $('#timer-pause').removeClass('disabled');
+    });
+
+    $('#timer-pause').click(function () {
+      // pause timer
+      $clock.countdown('pause');
+
+      pauseMin = new Date();
+
+      $(this).addClass('disabled');
+      $('#timer-resume').removeClass('disabled');
+    });
+
+    $('#timer-reset').click(function () {
+
+      // reset timer
+      var duration = setTimers(sessTime);
+      $clock.countdown(duration);
+
+      $('#timer-resume').removeClass('disabled');
+      $('#timer-resume').removeClass('active');
+      $('#timer-pause').removeClass('disabled');
+      $('#timer-pause').removeClass('active');
+    });
+
+  }
+
+  /*
+  PANEL CONTROLS
+  */
+
+  // call immediate function to close panels
+  (function () {
+    $('.panel')
+      .find('.panel-body')
+      .slideUp();
+
+    $('.panel').children('.clickable')
+      .addBack()
+      .addClass('panel-collapsed');
+
+    $('.panel').children('.clickable')
+      .addBack()
+      .find('i')
+      .removeClass('glyphicon-chevron-up')
+      .addClass('glyphicon-chevron-down');
+  })();
+
+  // setup click eventS for panels
+  $(document).on('click', '.panel-heading, clickable', function (e) {
+
+    var $this = $(this);
+
+    if (!$this.hasClass('panel-collapsed')) {
+
+      panelClosed();
+
+    } else {
+
+      panelOpen();
+
+    }
+
+    //close
+    function panelClosed() {
+      $this.parents('.panel')
         .find('.panel-body')
         .slideUp();
 
-      $('.panel').children('.clickable')
+      $this.siblings('.clickable')
         .addBack()
         .addClass('panel-collapsed');
 
-      $('.panel').children('.clickable')
+      $this.siblings('.clickable')
         .addBack()
         .find('i')
         .removeClass('glyphicon-chevron-up')
         .addClass('glyphicon-chevron-down');
-    })();
+    }
 
-    // setup click event for panels
-    $(document).on('click', '.panel-heading, clickable', function (e) {
+    //open
+    function panelOpen() {
+      $this.parents('.panel')
+        .find('.panel-body')
+        .slideDown();
 
-      var $this = $(this);
+      $this.siblings('.clickable')
+        .addBack()
+        .removeClass('panel-collapsed');
 
-      if (!$this.hasClass('panel-collapsed')) {
+      $this.siblings('.clickable')
+        .addBack()
+        .find('i')
+        .removeClass('glyphicon-chevron-down')
+        .addClass('glyphicon-chevron-up');
+    }
+  });
 
-        panelClosed();
-
-      } else {
-
-        panelOpen();
-
-      }
-
-      function panelClosed() {
-        $this.parents('.panel')
-          .find('.panel-body')
-          .slideUp();
-
-        $this.siblings('.clickable')
-          .addBack()
-          .addClass('panel-collapsed');
-
-        $this.siblings('.clickable')
-          .addBack()
-          .find('i')
-          .removeClass('glyphicon-chevron-up')
-          .addClass('glyphicon-chevron-down');
-      }
-
-      function panelOpen() {
-        $this.parents('.panel')
-          .find('.panel-body')
-          .slideDown();
-
-        $this.siblings('.clickable')
-          .addBack()
-          .removeClass('panel-collapsed');
-
-        $this.siblings('.clickable')
-          .addBack()
-          .find('i')
-          .removeClass('glyphicon-chevron-down')
-          .addClass('glyphicon-chevron-up');
-      }
-    });
-  }
 });
