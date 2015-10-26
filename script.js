ce4ab21e0b4bea6781bdb3246cf0d2d85221aac3
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-26 13:02:04 -0500

    Stable layout preparing for branching & tagging.

--- script.js
+++ script.js
@@ -1,80 +1,66 @@
 /* global $ */
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var resumeMin;
-  var pauseMin;
-  var duration = new Date();
-
-  // set url for sounds on google drive public
-  var soundLocation = 'https://7b313a9f7606490ebe3c2c7d078512212b272396.googledrive.com/';
-  soundLocation += 'host/0BxHL3kJgWo5eT1NpOW8zbzB0SzQ/sounds/';
-
-  $.ionSound({
-    sounds: [
-      {
-        name: 'bell_ring'
-      },
-      {
-        name: 'snap'
-      }
-    ],
-    volume: 0.1,
-    multiplay: true,
-    path: soundLocation,
-    preload: true
-  });
-
+  // timer selector
   var $clock = $('.timer');
 
-  // initially set default timer variables
-  var sessTime = 25;
-  var sessBreak = 1;
-  var setTime = 1;
-  var setBreak = 1;
+  // setup time pause variables
+  var startTime;
+  var resumeMin;
+  var pauseMin;
+  var timerMins = new Date();
+  var timerMs;
+
+  // initially set pomodoro variables
+  var setUnits = 3;
+  var sessTime = 1;
+  var sessBreakTime = 5;
+  var setBreakTime = 15;
 
   /*
-  MAIN POMODORO DRIVER
+  POMODORO CALL MAIN DRIVER
   */
-  setPomodoro(sessTime, sessBreak, setTime, setBreak);
+
+  timerMins = convertValueToMinutes(sessTime);
+  timerMs = convertValueToMs(sessTime);
+  setUpPomodoro(timerMins, timerMs);
 
   /*
-  TIMER SET FUNCTION
+  POMODORO SETUP FUNCTION
   */
-  function setPomodoro(sessTime, sessBreak, setTime, setBreak) {
-
-    duration = setTimers(sessTime);
-
-    //  compute session time
-    function setTimers() {
-      var currentTime = new Date();
-      var timerTime = new Date();
-      return timerTime.setMinutes(currentTime.getMinutes() + sessTime);
-    }
+  function setUpPomodoro(timerMins, timerMs) {
 
     /*
     TIMER EVENTS
     */
 
-    $clock.countdown(duration, function (event) {
-      $(this).
+    $clock.countdown(timerMins, function(event) {
+
+        $(this).
         html(event.strftime('<span>%M</span> min ' + '<span>%S</span> sec'));
-    })  //finish
-      .on('finish.countdown', function (event) {
-        $.ionSound.play('bell_ring', { volume: 0.1, loop: 5 });
-      })//stop
-    // .on('stop.countdown', function (event) {
-    //   $.ionSound.play('bell_ring', { volume: 0.1, loop: 5 });
-    // })//update
-      .on('update.countdown', function (event) {
-        $.ionSound.play('snap', { volume: 0.25 });
+
+      }) //finish
+      .on('finish.countdown', function(event) {
+        $.ionSound.play('bell_ring', {
+          volume: 0.1,
+          loop: 5
+        });
+      }) //stop
+      // .on('stop.countdown', function (event) {
+      //   $.ionSound.play('bell_ring', { volume: 0.1, loop: 5 });
+      // })//update
+      .on('update.countdown', function(event) {
+        $.ionSound.play('snap', {
+          volume: 0.25
+        });
       });
 
     /*
     TIMER CONTROLS
     */
 
-    $('#timer-resume').click(function () {
+    $('#timer-resume').click(function() {
       //resume timer
       //$clock.countdown('resume');
 
@@ -89,7 +75,7 @@ $(document).ready(function () {
       $('#timer-pause').removeClass('disabled');
     });
 
-    $('#timer-pause').click(function () {
+    $('#timer-pause').click(function() {
       // pause timer
       $clock.countdown('pause');
 
@@ -99,10 +85,10 @@ $(document).ready(function () {
       $('#timer-resume').removeClass('disabled');
     });
 
-    $('#timer-reset').click(function () {
+    $('#timer-reset').click(function() {
 
       // reset timer
-      var duration = setTimers(sessTime);
+      var duration = convertValueToMinutes(sessTime);
       $clock.countdown(duration);
 
       $('#timer-resume').removeClass('disabled');
@@ -114,11 +100,31 @@ $(document).ready(function () {
   }
 
   /*
+  CONVERT VALUES TO MINUTES
+  */
+  function convertValueToMinutes(timerValue) {
+
+    startTime = new Date();
+    var currentTime = startTime;
+    var timerTime = startTime;
+
+    return timerTime.setMinutes(currentTime.getMinutes() + timerValue);
+  }
+
+  /*
+  CONVERT VALUES TO MS
+  */
+  function convertValueToMs(timerValue) {
+    timerMs = timerValue * 60 * 1000;
+    return timerMs;
+  }
+
+  /*
   PANEL CONTROLS
   */
 
   // call immediate function to close panels
-  (function () {
+  (function() {
     $('.panel')
       .find('.panel-body')
       .slideUp();
@@ -135,7 +141,7 @@ $(document).ready(function () {
   })();
 
   // setup click eventS for panels
-  $(document).on('click', '.panel-heading, clickable', function (e) {
+  $(document).on('click', '.panel-heading, clickable', function(e) {
 
     var $this = $(this);
 
@@ -182,6 +188,24 @@ $(document).ready(function () {
         .removeClass('glyphicon-chevron-down')
         .addClass('glyphicon-chevron-up');
     }
+  });
+
+  /*
+  SET IONSOUND CONFIG
+  */
+  var soundLocation = 'https://7b313a9f7606490ebe3c2c7d078512212b272396.googledrive.com/';
+  soundLocation += 'host/0BxHL3kJgWo5eT1NpOW8zbzB0SzQ/sounds/';
+
+  $.ionSound({
+    sounds: [{
+      name: 'bell_ring'
+    }, {
+      name: 'snap'
+    }],
+    volume: 0.1,
+    multiplay: true,
+    path: soundLocation,
+    preload: true
   });
 
 });
