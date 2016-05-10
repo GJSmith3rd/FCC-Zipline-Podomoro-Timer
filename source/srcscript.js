219c3a22624816bf3d457198a7bde5dffe1283d9
Gilbert Smith <gjsmith3rd@gmail.com>
2016-05-09 19:04:39 -0500

    Updating for cloudinary jquery dyanmic reponsive images.

--- source/srcscript.js
+++ source/srcscript.js
@@ -2,21 +2,44 @@
 
 $(document).ready(function () {
 
-var developmentMode = false;
+  var stopGoogleAds = true;
 
-var stopGoogleAds = false;
+  //Test for local dev network
+  if (/^10.0.0/.test(location.hostname) && stopGoogleAds) {
 
-    //Test for local dev network
-    if (/^10.0.0/.test(location.hostname) && stopGoogleAds) {
+    // Remove Adsense from DOM
+    $('.adsense').remove();
 
-        // Remove Adsense from DOM
-        $('.adsense').remove();
+  } else {
 
-    } else {
+    // Load Adsense JavaScript
+    $.getScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+  }
 
-        // Load Adsense JavaScript
-        $.getScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
-    }
+  /*
+  * Cloudinary jQuery/JS Dynamic Images
+  */
+
+  (function () {
+
+    var bG = 'http://res.cloudinary.com/mobilecreature/image/upload/f_auto/v1456547839/FreeCodeCamp/Ziplines/Pomodoro-Timer/IeZm7MT.jpg';
+
+    var cBg = 'IeZm7MT.jpg';
+
+    $.cloudinary.config(
+      {
+        cloud_name: 'mobilecreature',
+        api_key: '222854649779236'
+      }
+    );
+
+    // $('.jumbotron').css('background-image', 'url("http://res.cloudinary.com/mobilecreature/image/upload/f_auto/v1456547839/FreeCodeCamp/Ziplines/Pomodoro-Timer/IeZm7MT.jpg")');
+
+    //$('.jumbotron').css('background-image', 'url(' + bG + ')');
+
+    //$('.jumbotron').attr('data-src', 'url(' + bG + ')');
+
+  })();
 
   // timer default setup
   var sessionValue = 25;
